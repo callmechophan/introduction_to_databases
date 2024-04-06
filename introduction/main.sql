fa2080c7ad4d16bd7fe3d2fc57a29347fae64f89
Cho Phan <callmechophan@gmail.com>
2024-04-06 21:39:21 +0700

    1.1 introduction

---
+++ introduction/main.sql
@@ -0,0 +1,11 @@
+/*
+ * database
+ * - a form of electronic storage in which data is held
+ * 
+ * database tasks
+ * - store data
+ * - form relationships
+ * - filter data
+ * - search data
+ * - perform CRUD operations
+*/
