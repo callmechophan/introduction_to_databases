c66b0a55c1cfbc4f3103d75234595c0585df9061
Cho Phan <callmechophan@gmail.com>
2024-04-06 23:55:56 +0700

    1.3 introduction to sql

---
+++ introduction_to_sql/main.sql
@@ -0,0 +1,52 @@
+/*
+ * SQL
+ * - standard language that can interact with structured data on databases
+ * 
+ * SQL and relational databases
+ * - MYSQL
+ * - PostgreSQL
+ * - Oracle
+ * - Microsoft SQL Server
+ * 
+ * SQL subsets
+ * - Data Definition Language (DDL)
+ * - Data Manipulation Language (DML)
+ * - Data Query Language (DQL)
+ * - Data Control Language (DCL)
+ * 
+ * SQL advantages
+ * - CRUD operators
+ * - retrieve and share data
+ * - database management and administration
+ * 
+ * create a database
+ * CREATE DATABASE database_name;
+ * 
+ * create a table
+ * CREATE TABLE table_name;
+ * 
+ * drop a table
+ * DROP TABLE table_name;
+ * 
+ * truncate a table
+ * TRUNCATE TABLE table_name;
+ * 
+ * add data to a table
+ * INSERT INTO table_name (column1, column2, column3) VALUES
+ * (value1, value2, value3);
+ * 
+ * update data in a table
+ * UPDATE table_name
+ * SET column_name = value
+ * WHERE condition;
+ * 
+ * delete data from a table
+ * DELERE FROM table_name
+ * WHERE condition;
+ * 
+ * retrieve data
+ * SELECT
+ *     column1,
+ *     column2
+ * FROM table_name;
+*/
