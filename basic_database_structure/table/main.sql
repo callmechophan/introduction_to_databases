d2534b5a87aeedc9a40db8fd1a2128a9bd56fb83
Cho Phan <callmechophan@gmail.com>
2024-04-07 03:22:47 +0700

    1.4.1 table

---
+++ basic_database_structure/table/main.sql
@@ -0,0 +1,12 @@
+/*
+ * table
+ * - is made up of rows and columns which hold data
+ * - and in a database that holds multiple tables
+ * 
+ * columns
+ * - also called fields or attributes
+ * - each column has a unique name and data type
+ * 
+ * rows
+ * - also called records
+*/
