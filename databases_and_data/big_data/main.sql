6e20f282acf2b23c4ed6e933c90b7f3e56d98337
Cho Phan <callmechophan@gmail.com>
2024-04-06 22:56:07 +0700

    1.2.3 big data

---
+++ databases_and_data/big_data/main.sql
@@ -0,0 +1,26 @@
+/*
+ * relational databases
+ * - used for storing structured data in tabular format
+ * 
+ * NoSQL databases
+ * - provide a flexible structure for storing and scaling data
+ * 
+ * types of NoSQL databases
+ * - document databases
+ * - key-value databases
+ * - graph databases
+ * 
+ * big data
+ * - complex data that grows exponentially with time
+ * 
+ * big data
+ * - contains a wide variety of data, arriving in increasing volumes and at high velocity
+ *     + variety
+ *     + volumes
+ *     + velocity
+ *  
+ * big data
+ * - combination of structured, semi-structured and unstructured data
+ * - more powerful than traditional data when solving problems
+ * - provides unique insights to help improve decision making
+*/
