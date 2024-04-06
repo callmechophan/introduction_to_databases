ef084e2bdb56621a245b4b6337e5297aab51e941
Cho Phan <callmechophan@gmail.com>
2024-04-06 22:06:33 +0700

    1.2.1 databases

---
+++ databases_and_data/databases/main.sql
@@ -0,0 +1,8 @@
+/*
+ * database
+ * - is a form of electronic storage in which data is organized systematically
+ * 
+ * stored
+ * - dedicated machine: host a database on-site
+ * - cloud hosting: host a database on the cloud
+*/
