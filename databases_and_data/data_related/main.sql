c51913266a576de3d2def2b182bb0ddf7449e631
Cho Phan <callmechophan@gmail.com>
2024-04-06 22:20:16 +0700

    1.2.2 data related

---
+++ databases_and_data/data_related/main.sql
@@ -0,0 +1,13 @@
+/*
+ * related
+ * - data stored in a database cannot exist in isolation, it must have a relationship with other data
+ * 
+ * primary key field
+ * - it contains unique values that cannot be replicated elsewhere in the table
+ * 
+ * foreign key field
+ * - is a field in one table that connects to the primary key field in the original table
+ * 
+ * relationship
+ * - the primary key of the table, but it becomes a foreign key of another table, the relationship is established and the data in these two table are related
+*/
